ef25a260b9bcb52e8dc799e3c042db15cf7a42bc
rdesc <rodrigue.de.schaetzen@gmail.com>
2020-06-01 15:44:15 -0700

    adds the tanh layers, outputs look more reasonable now

--- autorally_control/src/path_integral/scripts/get_nn_output.py
+++ autorally_control/src/path_integral/scripts/get_nn_output.py
@@ -1,6 +1,5 @@
 import numpy as np
 import torch
-import torch.optim as optim
 import torch.nn as nn
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
@@ -18,8 +17,10 @@ class AutoRallyDataset(Dataset):
 
 
 def make_data_loader():
-    data = np.full((1, 6), 0)
-    data[0] = [0, 6, 0, 0, 0, 1]
+    data = np.full((2, 6), 0)
+    # init all state variables to 0
+    data[0] = [0., 0., 0., 0., 0., 1.]
+    data[1] = [0., 0., 0., 0., 0., 2.]
     dataset = AutoRallyDataset(data)
     return DataLoader(dataset, batch_size=1, shuffle=False, num_workers=1, drop_last=False)
 
@@ -27,17 +28,18 @@ def make_data_loader():
 def load_model(f):
     # setup model architecture
     model = nn.Sequential(nn.Linear(6, 32),
+                          nn.Tanh(),
                           nn.Linear(32, 32),
+                          nn.Tanh(),
                           nn.Linear(32, 4))
-    print(model)
     # load npz file
     layers = np.load(f)
     bias_names = ["dynamics_b1", "dynamics_b2", "dynamics_b3"]
     layer_names = ["dynamics_W1", "dynamics_W2", "dynamics_W3"]
     # load weights and biases into appropriate layers
     for i in range(3):
-        model[i].bias = nn.Parameter(torch.from_numpy(layers[bias_names[i]]).float(), requires_grad=False)
-        model[i].weight = nn.Parameter(torch.from_numpy(layers[layer_names[i]]).float(), requires_grad=False)
+        model[i*2].bias = nn.Parameter(torch.from_numpy(layers[bias_names[i]]).float(), requires_grad=False)
+        model[i*2].weight = nn.Parameter(torch.from_numpy(layers[layer_names[i]]).float(), requires_grad=False)
     # load model onto GPU
     model.to(device)
 
@@ -46,11 +48,11 @@ def load_model(f):
 
 # input: roll, longitudinal velocity, lateral velocity, heading rate (state variables) and steering + throttle
 # output: time derivative of state variables
-def generate_output():
+def generate_output(f):
     # get a data loader
     data_loader = make_data_loader()
     # load model with pre-trained weights
-    model = load_model(file_path)
+    model = load_model(f)
     # init variable to store neural network outputs
     output = np.zeros(10)
     with torch.no_grad():
@@ -60,6 +62,8 @@ def generate_output():
             print(samples)
             y_pred = model(samples.float().to(device))
             print(y_pred)
+            y_pred = y_pred.detach().cpu().numpy()
+            print(y_pred)
 
 
 if __name__ == '__main__':
@@ -67,5 +71,6 @@ if __name__ == '__main__':
     if torch.cuda.is_available():
         device = torch.device('cuda')
 
-    file_path = "../params/models/autorally_nnet_09_12_2018.npz"
-    generate_output()
+    file_paths = ["../params/models/autorally_nnet_09_12_2018.npz"]  # ,"../params/models/gazebo_nnet_09_12_2018.npz"]
+    for f in file_paths:
+        generate_output(f)
